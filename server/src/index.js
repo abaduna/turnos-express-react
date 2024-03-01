a8ffb45536ad11912fe21f04ef67e1c619aa37e6
abaduna~ <artuelrey@gmail.com>
2024-03-01 11:00:42 -0300

    inyecion de sql

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,12 @@ app.use(express.json());
 
 //turnos disposibles
 app.get("/api/turnos/:medico", async (req, res) => {
+  
   const medico = req.params.medico;
+  const regex = /\s/;
+  if (regex.test(medico)) {
+    res.send(400).json({error:"no debe contener espacio"})
+  }
   const connection = await database.getConnection();
   if (medico == null) {
     res.status(500).json({ error: "medico is null or undefined" });
@@ -23,10 +28,9 @@ app.get("/api/turnos/:medico", async (req, res) => {
   }
   try {
     //sensible a inyercion de sql
-    const result = await connection.query(
-      'SELECT * FROM ?? WHERE estado = 0',
-      [medico]
-    );
+    const result = await connection.query("SELECT * FROM ?? WHERE estado = 0", [
+      medico,
+    ]);
     res.status(200).json(result);
   } catch (error) {
     console.error("Error en la consulta:", error);
@@ -41,6 +45,14 @@ app.post("/api/reservaturno/:id/:medico", async (req, res) => {
   const id = req.params.id;
   const connection = await database.getConnection();
   let { nombre, hora } = req.body;
+  const regex = /\s/;
+  if (regex.test(medico)) {
+    res.send(400).json({error:"no debe contener espacio"})
+  }
+  if (regex.test(nombre)) {
+    res.send(400).json({error:"no debe contener espacio"})
+  }
+  
   try {
     const result = await connection.query(
       `UPDATE ${medico} SET nombre = ?, estado = 1 WHERE id=?;`,
@@ -57,6 +69,10 @@ app.post("/api/turnosagregados/:medico", async (req, res) => {
   const medico = req.params.medico;
   const connection = await database.getConnection();
   let { hora } = req.body;
+  const regex = /\s/;
+  if (regex.test(medico)) {
+    res.send(400).json({error:"no debe contener espacio"})
+  }
   try {
     const result = await connection.query(
       `INSERT INTO ${medico} ( hora, estado) VALUES (?, ?);`,
@@ -73,6 +89,7 @@ app.post("/api/turnosagregados/:medico", async (req, res) => {
 //get de tabla de medicos
 app.get("/api/medicos", async (req, res) => {
   const connection = await database.getConnection();
+  
   try {
     const result = await connection.query("SELECT * FROM tabledemeidcos");
     res.status(200).json(result);
@@ -84,30 +101,31 @@ app.get("/api/medicos", async (req, res) => {
 });
 
 app.post("/api/cargarmedico", async (req, res) => {
-    const connection = await database.getConnection();
-    let { nameMedico } = req.body;
-    const sanitizedMedicoName = nameMedico.replace(/[^a-zA-Z0-9_]/g, '_');
-    
-    try {
-      const sentenciaSql = `CREATE TABLE ${sanitizedMedicoName} (id INT NOT NULL AUTO_INCREMENT, nombre VARCHAR(50) DEFAULT NULL,hora INT DEFAULT NULL,estado TINYINT DEFAULT 0,PRIMARY KEY ( id ));`;
+  const connection = await database.getConnection();
+  let { nameMedico } = req.body;
   
-      await connection.query(sentenciaSql);
-      try {
-        await connection.query(`INSERT INTO tabledemeidcos (medico) VALUES (?);`, [sanitizedMedicoName]);
-        res.status(200).json({ message: "medico agregando" });
-      } catch (error) {
-        console.log(`error en la sentencia de insert`);
-        console.error(error);
-        res.status(500).json({ error: "Error en el put" });
-      }
-      
+  const sanitizedMedicoName = nameMedico.replace(/[^a-zA-Z0-9_]/g, "_");
+
+  try {
+    const sentenciaSql = `CREATE TABLE ${sanitizedMedicoName} (id INT NOT NULL AUTO_INCREMENT, nombre VARCHAR(50) DEFAULT NULL,hora INT DEFAULT NULL,estado TINYINT DEFAULT 0,PRIMARY KEY ( id ));`;
+
+    await connection.query(sentenciaSql);
+    try {
+      await connection.query(
+        `INSERT INTO tabledemeidcos (medico) VALUES (?);`,
+        [sanitizedMedicoName]
+      );
+      res.status(200).json({ message: "medico agregando" });
     } catch (error) {
-      console.error("Error en la consulta:", error);
+      console.log(`error en la sentencia de insert`);
+      console.error(error);
       res.status(500).json({ error: "Error en el put" });
     }
-  });
-
-
+  } catch (error) {
+    console.error("Error en la consulta:", error);
+    res.status(500).json({ error: "Error en el put" });
+  }
+});
 
 app.listen(3001, () => {
   console.log(`corriendo por el puerto 3000`);
